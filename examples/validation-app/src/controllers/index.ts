bf023e8e843881ce7bab02f70a411d02c7b9fbb9
Diana Lau <dhmlau@ca.ibm.com>
2020-03-13 23:35:54 -0400

    feat(example-validation-app): add validation example

---
+++ examples/validation-app/src/controllers/index.ts
@@ -0,0 +1,7 @@
+// Copyright IBM Corp. 2020. All Rights Reserved.
+// Node module: @loopback/example-validation-app
+// This file is licensed under the MIT License.
+// License text available at https://opensource.org/licenses/MIT
+
+export * from './ping.controller';
+export * from './coffee-shop.controller';
---
+++ examples/validation-app/src/index.ts
@@ -0,0 +1,21 @@
+// Copyright IBM Corp. 2020. All Rights Reserved.
+// Node module: @loopback/example-validation-app
+// This file is licensed under the MIT License.
+// License text available at https://opensource.org/licenses/MIT
+
+import {ValidationApplication} from './application';
+import {ApplicationConfig} from '@loopback/core';
+
+export {ValidationApplication};
+
+export async function main(options: ApplicationConfig = {}) {
+  const app = new ValidationApplication(options);
+  await app.boot();
+  await app.start();
+
+  const url = app.restServer.url;
+  console.log(`Server is running at ${url}`);
+  console.log(`Try ${url}/ping`);
+
+  return app;
+}
